ce63b798cb3e890fb0347961237949f0fa1d31e2
Akira <54892411+docxed@users.noreply.github.com>
2022-03-18 23:35:47 +0700

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,65 @@
+import { createWebHistory, createRouter } from "vue-router"
+import Index from "@/pages/index.vue"
+import Login from "@/pages/login.vue"
+import Register from "@/pages/register.vue"
+import Addbedsforsell from "@/pages/addbedsforsell.vue"
+import Profile from "@/pages/profile.vue"
+import Findbeds from "@/pages/findbeds.vue"
+import Buybeds from "@/pages/buybeds.vue"
+import Beds from "@/pages/beds.vue"
+import Bedsdetail from "@/pages/bedsdetail.vue"
+
+const routes = [
+  {
+    path: "/",
+    name: "index",
+    component: Index,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register,
+  },
+  {
+    path: "/addbedsforsell",
+    name: "addbedsforsell",
+    component: Addbedsforsell,
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    component: Profile,
+  },
+  {
+    path: "/findbeds",
+    name: "findbeds",
+    component: Findbeds,
+  },
+  {
+    path: "/buybeds/:id",
+    name: "buybeds",
+    component: Buybeds,
+  },
+  {
+    path: "/beds",
+    name: "beds",
+    component: Beds,
+  },
+  {
+    path: "/bedsdetail/:id",
+    name: "bedsdetail",
+    component: Bedsdetail,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
